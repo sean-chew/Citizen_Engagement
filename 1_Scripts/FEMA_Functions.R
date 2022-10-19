4b335bde5f8ae0424d5008806f871a30497ef758
sean-chew <sean.chew@columbia.edu>
2022-10-18 23:26:37 -0400

    Continued work on final FEMA maps

--- 1_Scripts/FEMA_Functions.R
+++ 1_Scripts/FEMA_Functions.R
@@ -34,3 +34,59 @@ time_plotting_series <- function(data,title){
         scale_y_continuous(label=comma)
     return(plot)
 }
+# tm_shape(nyc_raster) +
+# tm_rgb() +
+final_plotting<-function(data,title,variable){
+        plot<- tm_shape(data, unit = "imperial")+
+            tm_fill(variable, 
+                    palette = "PuBu", lwd = .01,alpha = .3,
+                    style = "jenks") +
+            tm_compass(size = .8) +
+            tm_scale_bar(text.size = .5)+
+            tm_layout(title = title,
+                      title.color = "white",
+                      title.size = 1,
+                      legend.text.color = "white",
+                      bg.color = "darkgrey", 
+                      frame = FALSE)
+    tmap_save(plot,paste0("../4_Outputs/2_FEMA_Charts/",str_replace_all(title, " ", ""),".pdf"))
+}
+
+zipcode_analysis <- function(zipcode,data,storm_points,title,buffer){
+    processed <-  data %>%
+        filter(ZIPCODE %in% zipcode) %>% 
+        st_transform(crs = 2263)
+    
+    pluto <- map_pluto %>% 
+        st_intersection(processed %>% st_buffer(buffer))
+    
+    parks <- parks %>% 
+        st_transform(crs = 2263) %>% 
+        st_intersection(processed %>% st_buffer(buffer))
+    
+    storm_contour_zip <- storm_points %>% 
+        filter(incident_zip %in% zipcode) %>% 
+        st_kde() %>% 
+        st_get_contour(cont = c(10,20,30,40,50,60,70,80,90,100)) %>% 
+        st_intersection()
+    
+    flooding_crop <- st_intersection(extreme_flood,processed %>% st_buffer(buffer))
+    
+    final_map <- tm_shape(parks) +
+        tm_fill(col = "#99C2A2") + 
+        tm_shape(pluto)+
+        tm_borders("grey", alpha = .3, lwd = .5) +
+        tm_shape(flooding_crop)+
+        tm_fill(col = "#1D84B5")+
+        tm_shape(storm_contour_zip,is.master = TRUE)+
+        tm_fill(title = "Percentile","contlabel",
+                palette = "PuBu", lwd = .01,alpha = .3) +
+        tm_layout(title = title, legend.outside = TRUE,bg.color = "#132E32") +
+        tm_scale_bar(position = c("left", "bottom"), width = 0.15) +
+        tm_compass(position = c("left", "top"), size = 1)
+    
+    #print(paste0("../4_Outputs/2_FEMA_Charts/",str_replace_all(title, " ", ""),".pdf"))
+    tmap_save(final_map,paste0("../4_Outputs/2_FEMA_Charts/",str_replace_all(title, " ", ""),".pdf"))
+              
+    return(final_map)
+}
